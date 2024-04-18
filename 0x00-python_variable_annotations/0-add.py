ed38736be1d28de167147b35ee3af33a34f6c297
raxidbou4nja <rachid.boughnja@gmail.com>
2024-04-18 10:13:06 +0000

    add new file

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 0. Basic annotations - add
 """
 
 
 def add(number1: float, number2: float) -> float:
-    return [number2 + number2]
+    return (number2 + number2)
