a47ff454d7ccd847df3811507d416abce86144b8
raxidbou4nja <125773922+raxidbou4nja@users.noreply.github.com>
2024-04-23 13:02:31 +0100

    Update 2-measure_runtime.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,35 +1,18 @@
 #!/usr/bin/env python3
 """ Run time for four parallel comprehensions """
 import asyncio
-import random
 import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def async_generator():
-    """
-        async_generator function: without args
-    """
-    for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.uniform(0, 10)
-
-
-async def async_comprehension():
-    """
-        async_comprehension function: without args
-    """
-    async_numbers = [num async for num in async_generator()]
-    return async_numbers
-
-
-async def measure_runtime():
+async def measure_runtime() -> float:
     """
         measure_runtime function: without args
     """
     start_time = time.time()
     await asyncio.gather(async_comprehension(),
-                async_comprehension(),
-                async_comprehension(),
-                async_comprehension())
+                        async_comprehension(),
+                        async_comprehension(),
+                        async_comprehension())
     end_time = time.time()
     return end_time - start_time
