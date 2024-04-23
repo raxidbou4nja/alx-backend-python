cf3e42d6e200bfbd3766bc3af295ca2fa90d58ce
raxidbou4nja <125773922+raxidbou4nja@users.noreply.github.com>
2024-04-23 12:44:14 +0100

    Create 0-async_generator.py

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" 0. Async Generator """
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+       0. Async Generator
+
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
