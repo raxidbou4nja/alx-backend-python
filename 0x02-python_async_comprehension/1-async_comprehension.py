3aa63a641736e5bf9b4624cf3175f5bed454afb5
raxidbou4nja <125773922+raxidbou4nja@users.noreply.github.com>
2024-04-23 12:52:04 +0100

    Update 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,12 +6,16 @@ import random
 
 async def async_generator():
     """
-        async_comprehension that takes no arguments
+        async_generator that takes no arguments
     """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
 
+
 async def async_comprehension():
+    """
+        async_comprehension that takes no arguments
+    """
     async_numbers = [num async for num in async_generator()]
     return async_numbers
